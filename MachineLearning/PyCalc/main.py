1c5ca16a772c382f9f7370d7c528cf87b320636f
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-08-15 21:31:49 -0300

    Update main.py

--- MachineLearning/PyCalc/main.py
+++ MachineLearning/PyCalc/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
-import RedeNeural as rn
+import pandas as pd
+import RedeNeural
 
 st.markdown("<h1 style='text-align: center;'>Calculadora de Regressão Polinomial - CRP</h1>", unsafe_allow_html=True)
 st.markdown("## Entre com os pontos ou faça o upload de um arquivo CSV (pontos devem ser separados por um espaço)")
@@ -14,6 +15,8 @@ if "coordenadas" not in st.session_state:
 #    st.session_state.last_filled = False
 
 coordenadas = st.session_state.coordenadas
+st.session_state.parar = False
+parar = st.session_state.parar
 
 print(f"coordenadas na memória logo antes do expander: {coordenadas}")
 print(f"len do array de coordenadas: {len(coordenadas)}")
@@ -21,36 +24,54 @@ print(f"len do array de coordenadas: {len(coordenadas)}")
 def cheio(coord):
     return (coord["x"] != 0.0) or (coord["y"] != 0.0)
 
+st.sidebar.title("Instruções")
+with st.sidebar:
+    st.markdown("Carregue as coordenadas manualmente ou por meio de um arquivo csv.\n"
+            "## IMPORTANTE: Caso carregue o arquivo csv, as coordenadas manuais sumirão")
+
+#adiciona barra lateral de configurações
 st.sidebar.title("Configurações")
 with st.sidebar.expander("Parâmetros avançados"):
-    epochs = st.slider("quantidade de eras de treinamento", 3000, 10000, value=3000)
+    epochs = st.slider("quantidade de eras de treinamento", 100, 10000, value=2500)
+    tamanho_rede = st.slider("número de nodes na rede", 10, 50, value=25)
+
+# adiciona possibilidade de fazer upload de arquivo
+uploaded_file = st.file_uploader("Escolha um arquivo CSV", ".csv", accept_multiple_files=False)
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file, sep=r"[;,\s]+", engine="python", header=None)
+    print(df)
+    coordenadas = [{"x": float(row[0]), "y": float(row[1])} for row in df.to_numpy()]
+    st.session_state.coordenadas = coordenadas
+    print(st.session_state.coordenadas)
 
 # Interface de entrada
-with st.expander("Inserir pontos", expanded=True):
-    for i in range(len(coordenadas)):
-        col1, col2 = st.columns(2)
-        key_x = f"x_{i}"
-        key_y = f"y_{i}"
-        print(f"key_x no loop: {key_x}")
-        print(f"key_y no loop: {key_y}")
+if not uploaded_file:
+    with st.expander("Inserir pontos", expanded=True):
+        for i in range(len(coordenadas)):
+            col1, col2 = st.columns(2)
+            key_x = f"x_{i}"
+            key_y = f"y_{i}"
+            print(f"key_x no loop: {key_x}")
+            print(f"key_y no loop: {key_y}")
 
-        # Garante que as chaves estejam no session_state antes da criação dos widgets
-        if key_x not in st.session_state:
-            st.session_state[key_x] = coordenadas[i]["x"]
-        if key_y not in st.session_state:
-            st.session_state[key_y] = coordenadas[i]["y"]
+            # Garante que as chaves estejam no session_state antes da criação dos widgets
+            if key_x not in st.session_state:
+                st.session_state[key_x] = coordenadas[i]["x"]
+            if key_y not in st.session_state:
+                st.session_state[key_y] = coordenadas[i]["y"]
 
-        with col1:
-            st.number_input(f"X[{i+1}]", key=key_x, step=1.0, format="%.2f")
-        with col2:
-            st.number_input(f"Y[{i+1}]", key=key_y, step=1.0, format="%.2f")
+            with col1:
+                st.number_input(f"X[{i+1}]", key=key_x, step=1.0, format="%.2f")
+            with col2:
+                st.number_input(f"Y[{i+1}]", key=key_y, step=1.0, format="%.2f")
 
-    # Atualiza coordenadas com os valores atuais do session_state
-    for i in range(len(coordenadas)):
-        coordenadas[i]["x"] = st.session_state[f"x_{i}"]
-        coordenadas[i]["y"] = st.session_state[f"y_{i}"]
-        print(f"atualização de coordenada x no loop: {coordenadas[i]['x']}")
-        print(f"atualização de coordenada y no loop: {coordenadas[i]['y']}")
+        # Atualiza coordenadas com os valores atuais do session_state
+        for i in range(len(coordenadas)):
+            coordenadas[i]["x"] = st.session_state[f"x_{i}"]
+            coordenadas[i]["y"] = st.session_state[f"y_{i}"]
+            print(f"atualização de coordenada x no loop: {coordenadas[i]['x']}")
+            print(f"atualização de coordenada y no loop: {coordenadas[i]['y']}")
 
 st.markdown("""
     <style>
@@ -62,9 +83,6 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center;'>Adicione um arquivo aqui</h1>", unsafe_allow_html=True)
-uploaded_file = st.file_uploader("", ".csv", accept_multiple_files=False, )
-
 # Verifica se deve adicionar nova linha
 ultimo = coordenadas[-1]
 filled_now = cheio(ultimo)
@@ -72,7 +90,7 @@ filled_now = cheio(ultimo)
 print(f"último: {ultimo}")
 print(f"filled_now: {filled_now}")
 
-if filled_now:
+if filled_now and not uploaded_file:
     coordenadas.append({"x": 0.0, "y": 0.0})
     st.session_state[f"x_{len(coordenadas) - 1}"] = 0.0
     st.session_state[f"y_{len(coordenadas) - 1}"] = 0.0
@@ -82,6 +100,32 @@ if filled_now:
         st.number_input(f"Y[{len(coordenadas)}]", key=f"y_{len(coordenadas) -1}", step=1.0, format="%.2f")
 print(f"coordenadas no final: {coordenadas}")
 
-parar = st.checkbox("parar")
+#botão para começar a regressão
+if "iniciado" not in st.session_state:
+    st.session_state.iniciado = False
 
-rn.fit([{"x": x, "y": x ** 3} for x in np.linspace(-10, 10, 100)], 2500, parar=parar)
+if not st.session_state.iniciado:
+    iniciar = st.button("Iniciar", key="iniciar")
+
+    st.markdown("""
+        <style>
+        div.st-key-iniciar button {
+            background-color: green !important;
+            color: white !important;
+        }
+        div.st-key-iniciar button:hover {
+            box-shadow: 0px 4px 12px rgba(0, 255, 0, 0.6);
+            transform: scale(1.05);
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    if iniciar:
+        st.session_state.iniciado = True
+        print("[REDE NEURAL INICIADA]")
+        RedeNeural.fit([{"x": x, "y": x ** 3} for x in np.linspace(-10, 10, 100)], epochs)
+
+if st.button("Parar", key="botao_parar"):
+    st.session_state.parar = True
+    st.session_state.iniciado = False
+    st.rerun()
