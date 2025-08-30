59faa85d72e305dff074ab5150baf4c869c90b09
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-08-30 18:15:31 -0300

    Update main.py

--- MachineLearning/PyCalc/main.py
+++ MachineLearning/PyCalc/main.py
@@ -17,7 +17,7 @@ st.markdown("## Entre com os pontos ou faça o upload de um arquivo CSV (pontos 
 st.sidebar.title("Instruções")
 with st.sidebar:
     st.markdown("Carregue as coordenadas manualmente ou por meio de um arquivo csv.\n"
-            "## IMPORTANTE: Caso carregue o arquivo csv, as coordenadas manuais sumirão")
+            "## IMPORTANTE: Caso carregue o arquivo csv, as coordenadas manuais serão substituídas")
 
 #adiciona barra lateral de configurações
 st.sidebar.title("Configurações")
@@ -30,27 +30,9 @@ def criar_modelo():
     ])
     st.session_state.modelo.compile(optimizer=Adam(learning_rate=st.session_state.aprendizado), loss='mse')
 
-with st.sidebar.expander("Parâmetros avançados"):
-    st.session_state.funcao_ativacao = st.selectbox("Escolha a função de ativação", options=["relu", "tanh", "sigmoid", "leakyrelu", "linear"])
-    st.session_state.aprendizado = st.select_slider("Taxa de Aprendizado", [0.001, 0.01, 0.1], value=0.01)
-    epochs = st.slider("Quantidade de Eras de Treinamento", 100, 10000, value=2500)
-    tamanho_rede = st.slider("Número de Nodes na Rede", 5, 50, value=10)
-    st.button("Reconfigurar Modelo", on_click=criar_modelo)
-
-print(f"Função de ativação: {st.session_state.funcao_ativacao}")
-
 #estabelece variáveis do session_state
 if "coordenadas" not in st.session_state:
     st.session_state.coordenadas = [{"x": 0.0, "y": 0.0}]
-
-#cria a rede neural. É por isso que o parâmetro "tamanho da rede vem antes"
-if "modelo" not in st.session_state:
-    st.session_state.modelo = Sequential([
-        Dense(tamanho_rede, input_dim=1, activation="relu"),
-        Dense(tamanho_rede, activation="relu"),
-        Dense(1)
-    ])
-    st.session_state.modelo.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
 
 if "indice_epoca" not in st.session_state:
     st.session_state.indice_epoca = 0
@@ -73,6 +55,9 @@ if "mostrar_tabela" not in st.session_state:
 if "modelo treinado" not in st.session_state:
     st.session_state.modelo_treinado = False
 
+if "iniciado" not in st.session_state:
+    st.session_state.iniciado = False
+
 #Começa com arquivo nulo carregado
 uploaded_file = ""
 print(f"arquivo carregado no início da execução: {uploaded_file}")
@@ -83,6 +68,24 @@ parar = st.session_state.parar
 
 print(f"coordenadas na memória logo antes do expander: {coordenadas}")
 print(f"len do array de coordenadas: {len(coordenadas)}")
+
+with st.sidebar.expander("Parâmetros avançados"):
+    st.session_state.funcao_ativacao = st.selectbox("Escolha a função de ativação", options=["relu", "sigmoid", "linear", "softmax", "swish"])
+    st.session_state.aprendizado = st.select_slider("Taxa de Aprendizado", [0.001, 0.01, 0.1], value=0.01, disabled=st.session_state.iniciado)
+    epochs = st.slider("Quantidade de Eras de Treinamento", 100, 10000, value=2500, disabled=st.session_state.iniciado)
+    tamanho_rede = st.slider("Número de Nodes na Rede", 5, 50, value=10, disabled=st.session_state.iniciado)
+    st.button("Reconfigurar Modelo", on_click=criar_modelo, disabled=st.session_state.iniciado)
+
+print(f"Função de ativação: {st.session_state.funcao_ativacao}")
+
+#cria a rede neural. É por isso que o parâmetro "tamanho da rede vem antes"
+if "modelo" not in st.session_state:
+    st.session_state.modelo = Sequential([
+        Dense(tamanho_rede, input_dim=1, activation="relu"),
+        Dense(tamanho_rede, activation="relu"),
+        Dense(1)
+    ])
+    st.session_state.modelo.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
 
 # adiciona possibilidade de fazer upload de arquivo
 if st.session_state.mostrar_csv_dialog:
@@ -95,6 +98,33 @@ if st.session_state.mostrar_csv_dialog:
         coordenadas = [{"x": float(row[0]), "y": float(row[1])} for row in df.to_numpy()]
         st.session_state.coordenadas = coordenadas
         print(st.session_state.coordenadas)
+
+if not st.session_state.iniciado:
+
+    def iniciar():
+        if len(coordenadas) == 1:
+            st.warning("Nenhum dado foi inserido. Insira dados e tente novamente")
+            time.sleep(2)
+        st.session_state.iniciado = True
+        st.session_state.epochs = epochs
+        st.session_state.mostrar_csv_dialog = False
+        st.session_state.mostrar_tabela = False
+
+    iniciar = st.button("Iniciar", key="iniciar", on_click=iniciar)
+
+    #estabelece o estilo do botão de iniciar
+    st.markdown("""
+        <style>
+        div.st-key-iniciar button {
+            background-color: green !important;
+            color: white !important;
+        }
+        div.st-key-iniciar button:hover {
+            box-shadow: 0px 4px 12px rgba(0, 255, 0, 0.6);
+            transform: scale(1.05);
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
 # Interface de entrada
 if st.session_state.mostrar_tabela:
@@ -158,37 +188,6 @@ if filled_now and not uploaded_file and st.session_state.mostrar_tabela:
         st.number_input(f"Y[{len(coordenadas)}]", key=f"y_{len(coordenadas) -1}", step=1.0, format="%.2f")
 print(f"coordenadas no final: {coordenadas}")
 
-#botão para começar a regressão
-if "iniciado" not in st.session_state:
-    st.session_state.iniciado = False
-
-if not st.session_state.iniciado:
-
-    def iniciar():
-        if len(coordenadas) == 1:
-            st.warning("Nenhum dado foi inserido. Insira dados e tente novamente")
-            time.sleep(2)
-        st.session_state.iniciado = True
-        st.session_state.epochs = epochs
-        st.session_state.mostrar_csv_dialog = False
-        st.session_state.mostrar_tabela = False
-
-    iniciar = st.button("Iniciar", key="iniciar", on_click=iniciar)
-
-    #estabelece o estilo do botão de iniciar
-    st.markdown("""
-        <style>
-        div.st-key-iniciar button {
-            background-color: green !important;
-            color: white !important;
-        }
-        div.st-key-iniciar button:hover {
-            box-shadow: 0px 4px 12px rgba(0, 255, 0, 0.6);
-            transform: scale(1.05);
-        }
-        </style>
-    """, unsafe_allow_html=True)
-
 if st.session_state.iniciado:
 
     # Muda o estilo do botão de iniciar - Display = none
@@ -232,13 +231,21 @@ if st.session_state.iniciado:
 
         #função para parar no loop
         def parar():
+            st.session_state.indice_epoca = 0
             st.session_state.coordenadas.clear()
-            st.session_state.modelo_treinado = True
+            st.session_state.coordenadas = [{"x": 0.0, "y": 0.0}]
+            st.session_state.modelo_treinado = False
             st.session_state.mostrar_csv_dialog = True
             st.session_state.mostrar_tabela = True
             st.session_state.parar = True
             st.session_state.iniciado = False
             st.session_state.estado_treinamento = None
+            st.session_state.modelo = Sequential([
+                Dense(tamanho_rede, input_dim=1, activation=st.session_state.funcao_ativacao),
+                Dense(tamanho_rede, activation=st.session_state.funcao_ativacao),
+                Dense(1)
+            ])
+            st.session_state.modelo.compile(optimizer=Adam(learning_rate=st.session_state.aprendizado), loss='mse')
 
         if st.session_state.indice_epoca == 0:
             with button_placeholder:
@@ -246,21 +253,23 @@ if st.session_state.iniciado:
 
         while st.session_state.indice_epoca < st.session_state.epochs:
             parar = st.session_state.parar
-            st.session_state.estado_treinamento = RedeNeural.fit(st.session_state.modelo, coordenadas, epochs, tamanho_rede, st.session_state.indice_epoca, parar, plot_placeholder, progress_bar, status_placeholder)
+            st.session_state.estado_treinamento, perda = RedeNeural.fit(st.session_state.modelo, coordenadas, epochs, tamanho_rede, st.session_state.indice_epoca, parar, plot_placeholder, progress_bar, status_placeholder)
             print(f"y_pred: {st.session_state.estado_treinamento}")
             print(parar)
 
             st.session_state.indice_epoca = st.session_state.indice_epoca + 1
 
-            if st.session_state.indice_epoca == st.session_state.epochs:
-                st.session_state.modelo_treinado = True
-                print(f"modelo treinado: {st.session_state.modelo_treinado}")
-                break
-
+        button_placeholder.empty()
+        plot_placeholder.empty()
+        progress_bar.empty()
+        st.session_state.modelo_treinado = True
+        print(f"modelo treinado: {st.session_state.modelo_treinado}")
         st.session_state.coordenadas.clear()
         st.session_state.coordenadas = [{"x": 0.0, "y": 0.0}]
-        st.session_state.modelo_treinado = True
-        status_placeholder.markdown("<h3 style='text-align: center; color=green;'>Modelo Treinado!</h3>",unsafe_allow_html=True)
+        if perda <= 100:
+            status_placeholder.markdown("<h3 style='text-align:center; color:green;'>Modelo Treinado!</h3>",unsafe_allow_html=True)
+        if perda > 100:
+            status_placeholder.markdown(f"<h3 style='text-align:center; color:red;'>Modelo Treinado (perda: {perda:.2f})</h3>",unsafe_allow_html=True)
 
 if st.session_state.modelo_treinado:
 
@@ -274,6 +283,12 @@ if st.session_state.modelo_treinado:
         st.session_state.parar = True
         st.session_state.iniciado = False
         st.session_state.estado_treinamento = None
+        st.session_state.modelo = Sequential([
+        Dense(tamanho_rede, input_dim=1, activation=st.session_state.funcao_ativacao),
+        Dense(tamanho_rede, activation=st.session_state.funcao_ativacao),
+        Dense(1)
+        ])
+        st.session_state.modelo.compile(optimizer=Adam(learning_rate=st.session_state.aprendizado), loss='mse')
 
     x = st.number_input("Entre com o X para ser previsto:", value=0.0, step=1.0, format="%.2f")
     if x is not None:
