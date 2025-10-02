dbeb2728df624ca4c4b82f2fb718fb88b040906d
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-10-01 21:54:20 -0300

    Update main.py

--- MachineLearning/WebScrapper/main.py
+++ MachineLearning/WebScrapper/main.py
@@ -8,26 +8,27 @@ from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from tkinter import filedialog
 from PIL import Image
-from openpyxl import workbook
 
-# Configuração da chave de API da Gemini
+# Configuração da chave de API da Gemini. Colocar em variável de ambiente futuramente.
 genai.configure(api_key="")
+#Local do Webdriver. Automatizar o download dele futuramente?
 WEBDRIVER = os.path.join(os.getcwd(), "webdriver", "msedgedriver.exe")
 
-def iniciar():
+def iniciar() -> None:
     if not os.path.exists(os.path.join(os.getcwd(), "webdriver", "msedgedriver.exe")):
         print("Erro: Edge Driver não foi localizado. Ele deve estar localizado na pasta 'webdriver'")
     if not os.path.exists(os.path.join(os.getcwd(), "bancos de dados")):
         os.mkdir(os.path.join(os.getcwd(), "bancos de dados"))
 
-def obter_dados_texto(url_site: str) -> None:
+#Pega os dados do site usando selenium headless. É possível alterar a posição da janela usando o argumento comentado.
+def obter_dados_texto(url_site: str) -> str:
     options = EdgeOptions()
     options.add_argument("--headless")  # Modo headless
-    options.add_argument("--window-position=3000,0")
+    #options.add_argument("--window-position=3000,0")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    # Cria o objeto Service com o caminho do driver
+    # Cria o objeto Service com o caminho do driver. O local do executável está declarado na variável global "WEBDRIVER"
     service = EdgeService(executable_path=WEBDRIVER)
 
     # Inicia o driver com o Edge headless
@@ -40,16 +41,30 @@ def obter_dados_texto(url_site: str) -> None:
         time.sleep(1)
         return driver.page_source
     except Exception as e:
-        print(f"erro ao obter código da página: {e}")
+        return f"erro ao obter código da página: {e}"
 
-
+#
 def perguntar_a_ia_texto(texto: str, vars: str) -> str:
+    #Melhorar, tornar mais robusto. A ideia é pegar três saídas diferentes, comparar e retornar o mais frequente. No entanto, usar um sorted()
+    #melhora a robustez. Pode ser facilmente expandido para verificar mais do que três vezes.
+    def verificar_consistencia(respostas: list) -> str:
+        #atribui cada saída gerada pela IA para um vetor diferente.
+        auxa = respostas[0]
+        auxb = respostas[1]
+        auxc = respostas[2]
+        #Comparar aux e ver qual se repete mais. Ordena as listas
+        if auxa == auxb:
+            return auxa
+        else:
+            return auxc
+
     vars = vars.split("--")
     vars = ", ".join(vars)
     """Usa a IA Gemini para responder a uma pergunta baseada no texto fornecido."""
     model = genai.GenerativeModel('gemini-2.0-flash')
 
     # Prepara o prompt para a IA
+    # É sempre importante explicar o máximo possível e colocar exemplos nos prompts
     prompt = f"""
     Analise a página seguir.
     com base nela, crie uma tabela e a preencha com os seguintes dados: {vars}. Cada um desses dados deve representar
@@ -73,8 +88,12 @@ def perguntar_a_ia_texto(texto: str, vars: str) -> str:
     {texto}
     """
     try:
-        resposta = model.generate_content(prompt)
-        return resposta.text
+        resposta = []
+        for i in range(0, 3):
+            resposta.append(model.generate_content(prompt).text)
+            print(resposta)
+        lista_final = verificar_consistencia(resposta)
+        return lista_final
     except Exception as e:
         return f"Erro ao gerar conteúdo com a IA: {e}"
 
@@ -87,6 +106,7 @@ def perguntar_a_ia_imagem(caminho_imagem: str, vars: str) -> str:
     model = genai.GenerativeModel('gemini-2.5-flash-image-preview')
 
     # Prepara o prompt para a IA
+    #É sempre importante explicar o máximo possível e colocar exemplos nos prompts
     prompt = f"""
     Analise a imagem a seguir. Use o reconhecimento de imagem.
     com base nela, crie uma tabela e a preencha com os seguintes dados: {vars}. Cada um desses dados deve representar
@@ -113,11 +133,14 @@ def perguntar_a_ia_imagem(caminho_imagem: str, vars: str) -> str:
     except Exception as e:
         return f"Erro ao gerar conteúdo com a IA: {e}"
 
+#salva os dados em uma planilha
 def salvar_planilha(vars, dados) -> None:
     try:
+        #elimina os parêntesis no início e no final
         dados = dados.splitlines()[1:-1]
         print(dados)
         num_linhas = len(dados)
+        #extrai os argumentos
         vars = vars.split("--")[1:]
         print(vars)
         num_cols = len(vars)
@@ -132,12 +155,15 @@ def salvar_planilha(vars, dados) -> None:
                     ws.cell(row=linha+2, column=i+1, value=dados[linha].split("/")[i])
                 except Exception as e:
                     print(e)
+        #Salva a planilha com um nome único. Importante para não haver sobreposição de dados.
         wb.save(os.path.join(os.getcwd(), "bancos de dados", f"database-{time.strftime("%Y-%m-%d_%H-%M-%S")}.xlsx"))
         print("Dados salvos!")
     except Exception as e:
         print(f"Erro: {e}")
 
+#concatena planilhas para criar banco de dados únicos
 def cat_data(caminhos: list) -> None:
+    #o i elimina a primeira linha.
     i = 0
     wb_combinado = Workbook()
     ws = wb_combinado.active
@@ -154,49 +180,55 @@ def cat_data(caminhos: list) -> None:
 
 if _name_ == "_main_":
     iniciar()
-    op = input("O que deseja fazer?\n1) Extrair dados de um site\n2) Extrair dados de uma imagem\n3) concatenar dados\n")
-    if op == "1":
-        try:
-            url_do_site = input("entre com o site que quer extrair informações: ")
-            vars = input("quais variáveis deseja extrair? separe-as com '--'.\n ")
-            if not url_do_site.startswith("http://") and not url_do_site.startswith("https://"):
-                url_do_site = "https://" + url_do_site
-            nome_do_arquivo_pdf = url_do_site.replace("http://", "")
-            nome_do_arquivo_pdf = nome_do_arquivo_pdf.replace("https://", "")
-            nome_do_arquivo_pdf = nome_do_arquivo_pdf.replace("www.", "")
-            nome_do_arquivo_pdf = nome_do_arquivo_pdf.split(".")[0]
-            nome_do_arquivo_pdf = nome_do_arquivo_pdf + ".pdf"
+    #Loop infinito
+    while True:
+        op = input("O que deseja fazer?\n1) Extrair dados de um site\n2) Extrair dados de uma imagem\n3) concatenar dados\n")
+        #Para extrair de um site
+        if op == "1":
+            try:
+                url_do_site = input("entre com o site que quer extrair informações: ")
+                vars = input("quais variáveis deseja extrair? separe-as com '--'.\n ")
+                if not url_do_site.startswith("http://") and not url_do_site.startswith("https://"):
+                    url_do_site = "https://" + url_do_site
+                nome_do_arquivo_pdf = url_do_site.replace("http://", "")
+                nome_do_arquivo_pdf = nome_do_arquivo_pdf.replace("https://", "")
+                nome_do_arquivo_pdf = nome_do_arquivo_pdf.replace("www.", "")
+                nome_do_arquivo_pdf = nome_do_arquivo_pdf.split(".")[0]
+                nome_do_arquivo_pdf = nome_do_arquivo_pdf + ".pdf"
 
-            # 1. Converte o site para PDF
-            conteudo_do_site = obter_dados_texto(url_do_site)
+                # 1. Converte o site para PDF
+                conteudo_do_site = obter_dados_texto(url_do_site)
 
-            # 3. Faz a pergunta à IA com base no texto extraído
-            dados = perguntar_a_ia_texto(conteudo_do_site, vars)
-            print("\nResposta da IA:\n")
-            print(dados)
-            salvar_planilha(vars, dados)
-        except Exception as e:
-            print(f"Erro: {e}")
+                # 3. Faz a pergunta à IA com base no texto extraído
+                dados = perguntar_a_ia_texto(conteudo_do_site, vars)
+                print("\nResposta da IA:\n")
+                print(dados)
+                salvar_planilha(vars, dados)
+            except Exception as e:
+                print(f"Erro: {e}")
 
-    if op == "2":
-        try:
-            caminho_imagem = filedialog.askopenfile(initialdir=os.getcwd(), filetypes=[("Todos", "."), ("PDF", ".pdf"), ("JPG", ".jpg"), ("JPEG", ".jpeg"), ("PNG", ".png")], title="Escolha o arquivo de imagem")
-            vars = input("quais variáveis deseja extrair? separe-as com '--'.\n ")
-            caminho_imagem = caminho_imagem.name
-            dados = perguntar_a_ia_imagem(caminho_imagem, vars)
-            print("\nResposta da IA:\n")
-            print(dados)
-            print(f"caminho da imagem: {caminho_imagem}")
-            salvar_planilha(vars, dados)
-        except Exception as e:
-            print(f"Erro: {e}")
+        elif op == "2":
+            try:
+                caminho_imagem = filedialog.askopenfile(initialdir=os.getcwd(), filetypes=[("Todos", "."), ("PDF", ".pdf"), ("JPG", ".jpg"), ("JPEG", ".jpeg"), ("PNG", ".png")], title="Escolha o arquivo de imagem")
+                vars = input("quais variáveis deseja extrair? separe-as com '--'.\n ")
+                caminho_imagem = caminho_imagem.name
+                dados = perguntar_a_ia_imagem(caminho_imagem, vars)
+                print("\nResposta da IA:\n")
+                print(dados)
+                print(f"caminho da imagem: {caminho_imagem}")
+                salvar_planilha(vars, dados)
+            except Exception as e:
+                print(f"Erro: {e}")
 
-    if op == "3":
-        try:
-            caminhos = filedialog.askopenfilenames(initialdir=os.getcwd(), title="Escolha os arquivos")
-            if caminhos:
-                cat_data(caminhos)
-            else:
-                print("Nenhum arquivo foi selecionado.")
-        except Exception as e:
-            print(f"Erro: {e}")
+        elif op == "3":
+            try:
+                caminhos = filedialog.askopenfilenames(initialdir=os.getcwd(), title="Escolha os arquivos")
+                if caminhos:
+                    cat_data(caminhos)
+                else:
+                    print("Nenhum arquivo foi selecionado.")
+            except Exception as e:
+                print(f"Erro: {e}")
+
+        else:
+            print("Operação Inválida! Tente novamente.")
