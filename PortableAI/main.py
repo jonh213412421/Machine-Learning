e592d754f964dd5f3b1a411ab3eac4d0b4a2ad29
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-11-19 20:00:52 -0300

    Update main.py

--- PortableAI/main.py
+++ PortableAI/main.py
@@ -1,25 +1,94 @@
 import subprocess
 import os
 import time
+import tkinter as tk
+from tkinter import filedialog
+import sys
 
-env = os.environ.copy()
-env["PYTHONIOENCODING"] = "utf-8"
-env["LC_ALL"] = "C.UTF-8"
-env["OLLAMA_MODELS"] = os.path.join(os.getcwd(), "Ollama/.ollama")
-#O problema é esta porta
-env["OLLAMA_HOST"] = "127.0.0.1:55555"
+def iniciar() -> None:
+    try:
+        if not os.path.exists(os.path.join(os.getcwd(), "data")):
+            os.mkdir(os.path.join(os.getcwd(), "data"))
 
-ollama_exe = os.path.join(os.getcwd(), "Ollama", "ollama.exe")
+        if not os.path.isfile(os.path.join(os.getcwd(), "data", "paths.txt")):
+            with open(os.path.join(os.getcwd(), "data", "paths.txt"), "w") as arquivo:
+                arquivo.write("modelos:\n\n")
+
+    except Exception as e:
+        print(f"Erro ao iniciar: {e}")
 
 
-resposta = subprocess.run(
-    [ollama_exe, "run", "gemma3", "olá"],
-    input="", # stdin vazio se não precisar digitar mais nada
-    capture_output=True, # Captura stdout E stderr
-    text=True,
-    shell=False,
-    encoding="utf-8",
-    env=env
-)
+def adicionar_modelo() -> None:
+    try:
+        if not os.path.isdir(os.path.join(os.getcwd(), "data")):
+            os.mkdir(os.path.join(os.getcwd(), "data"))
+            modelo_path = filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
+            modelo_nome = os.path.basename(modelo_path)
+            if not os.path.isfile(os.path.join(os.getcwd(), "data", "paths.txt")):
+                with open(os.path.join(os.getcwd(), "data", "paths.txt"), "w") as arquivo:
+                    arquivo.write("modelos:\n\n")
+                    arquivo.write(f"md->{modelo_nome}:")
+                    arquivo.write("\n")
+                    arquivo.write(modelo_path)
+                    arquivo.write("\n")
 
-print(resposta.stdout.strip())
+        else:
+            modelo_path = filedialog.askopenfilename()
+            modelo_nome = os.path.basename(modelo_path)
+            with open(os.path.join(os.getcwd(), "data", "paths.txt"), "r") as arquivo:
+                linhas = arquivo.readlines()
+                for linha in linhas:
+                    print(linha)
+                    if linha.startswith(modelo_nome):
+                        return None
+            with open(os.path.join(os.getcwd(), "data", "paths.txt"), "a") as arquivo:
+                arquivo.write(f"md->{modelo_nome}:")
+                arquivo.write("\n")
+                arquivo.write(modelo_path)
+                arquivo.write("\n")
+
+    except Exception as e:
+        print(f"Erro ao adicionar modelo: {e}")
+
+def selecionar_modelo() -> str:
+    return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
+
+
+def fazer_prompt(modelo: str, prompt: str) -> None:
+    print("[RODANDO PROMPT]")
+
+    os.chdir(os.path.join(os.getcwd(), "Llama"))
+
+    # Comando final
+    cmd = f"llama-run.exe .\\Modelos\\{modelo} -p {prompt}'"
+
+    try:
+        subprocess.run(cmd,
+                       stdin=subprocess.DEVNULL,  # -> SE PEDIR QUALQUER INTERVENÇÃO DO USUÁRIO, ELE CANCELA!!
+                       stdout=sys.stdout,
+                       stderr=sys.stdout,
+                       encoding="utf-8",
+                       shell=True,
+                       text=True)
+    except Exception as e:
+        print(f"Erro ao executar o modelo: {e}")
+
+
+# iniciar()
+#adicionar_modelo()
+#fazer_prompt("iu", "oi")
+modelo = selecionar_modelo()
+modelo = os.path.basename(modelo)
+prompt = "você fala português?"
+fazer_prompt(modelo, prompt)
+
+
+
+
+
+
+
+
+
+
+
