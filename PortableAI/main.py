d29a21d0729585e93557715745a523db132680ba
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-11-18 22:32:58 -0300

    Update main.py

--- PortableAI/main.py
+++ PortableAI/main.py
@@ -1,10 +1,25 @@
 import subprocess
 import os
+import time
 
 env = os.environ.copy()
 env["PYTHONIOENCODING"] = "utf-8"
 env["LC_ALL"] = "C.UTF-8"
+env["OLLAMA_MODELS"] = os.path.join(os.getcwd(), "Ollama/.ollama")
+#O problema é esta porta
+env["OLLAMA_HOST"] = "127.0.0.1:55555"
 
-resposta = subprocess.Popen([r".\Ollama\ollama.exe", "run", "gemma3", "olá"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True, encoding="utf-8", env=env)
-output, error = resposta.communicate()
-print("Ollama Output:", output.strip())
+ollama_exe = os.path.join(os.getcwd(), "Ollama", "ollama.exe")
+
+
+resposta = subprocess.run(
+    [ollama_exe, "run", "gemma3", "olá"],
+    input="", # stdin vazio se não precisar digitar mais nada
+    capture_output=True, # Captura stdout E stderr
+    text=True,
+    shell=False,
+    encoding="utf-8",
+    env=env
+)
+
+print(resposta.stdout.strip())
