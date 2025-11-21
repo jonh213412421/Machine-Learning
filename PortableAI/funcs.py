520f45f159ef2717e7cfd60196b8455e7d4ac14a
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-11-21 01:38:08 -0300

    Update funcs.py

--- PortableAI/funcs.py
+++ PortableAI/funcs.py
@@ -5,11 +5,17 @@ import tkinter as tk
 from tkinter import filedialog
 import sys
 
-def selecionar_modelo() -> str:
-    return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
+def listar_modelos() -> str:
+    return os.listdir("./Llama/Modelos")
 
+def selecionar_modelo(opcao) -> str:
+    if opcao == "Adicionar modelo":
+        return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
 
-def fazer_prompt(modelo: str, prompt: str) -> None:
+def carregar_arquivo() -> str:
+    return filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("pdf", "*.pdf"), ("text files", "*.txt"),))
+
+def fazer_prompt(modelo: str, prompt: str) -> str:
     print("[RODANDO PROMPT]")
 
     os.chdir(os.path.join(os.getcwd(), "Llama"))
@@ -18,21 +24,42 @@ def fazer_prompt(modelo: str, prompt: str) -> None:
     cmd = f"llama-run.exe .\\Modelos\\{modelo} -p {prompt}'"
 
     try:
-        subprocess.run(cmd,
-                       stdin=subprocess.DEVNULL,  # -> SE PEDIR QUALQUER INTERVENÇÃO DO USUÁRIO, ELE CANCELA!!
-                       stdout=sys.stdout,
-                       stderr=sys.stdout,
-                       encoding="utf-8",
-                       shell=True,
-                       text=True)
+        resposta = subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL, #-> RETORNA NULO PARA QUALQUER INPUT SOLICITADO DO USUÁRIO
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            text=True,
+            encoding="utf-8"
+        )
+        for line in iter(resposta.stdout.readline, ''):
+            sys.stdout.write(line)  # envia para o terminal
+            sys.stdout.flush()  # garante que aparece imediatamente
+            yield line.rstrip()
+
+        resposta.stdout.close()
+        resposta.wait()
     except Exception as e:
         print(f"Erro ao executar o modelo: {e}")
+        return "Erro!"
 
-
+print(listar_modelos())
 # iniciar()
 #adicionar_modelo()
 #fazer_prompt("iu", "oi")
-modelo = selecionar_modelo()
-modelo = os.path.basename(modelo)
-prompt = "você fala português?"
-fazer_prompt(modelo, prompt)
+#modelo = selecionar_modelo()
+#modelo = os.path.basename(modelo)
+#prompt = "você fala português?"
+#fazer_prompt(modelo, prompt)
+
+
+
+
+
+
+
+
+
+
+
