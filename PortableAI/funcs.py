08d8fa34559f9e627a477a0d0abc732326276c26
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-11-21 19:26:14 -0300

    Update funcs.py

--- PortableAI/funcs.py
+++ PortableAI/funcs.py
@@ -1,9 +1,15 @@
 import subprocess
 import os
-import time
-import tkinter as tk
 from tkinter import filedialog
 import sys
+import PyPDF2
+
+def iniciar() -> None:
+    if not os.path.exists(os.path.join(os.getcwd(), 'Llama')) or not os.path.exists(os.path.join(os.getcwd(), 'ícones')):
+        return "Erro! Falha na integridade dos diretórios."
+
+    if not os.path.isfile(os.path.join(os.getcwd(), 'Llama', 'llama-run.exe')):
+        return "Erro! llama-run.exe não está presente no diretório Llama."
 
 def listar_modelos() -> str:
     return os.listdir("./Llama/Modelos")
@@ -12,8 +18,26 @@ def selecionar_modelo(opcao) -> str:
     if opcao == "Adicionar modelo":
         return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
 
-def carregar_arquivo() -> str:
-    return filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("pdf", "*.pdf"), ("text files", "*.txt"),))
+def carregar_arquivo() -> ((str, str)):
+    try:
+        caminho = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("pdf", "*.pdf"), ("text files", "*.txt"),))
+        if caminho:
+            if caminho.endswith(".pdf"):
+                with open(caminho, "rb") as arquivo:
+                    pdf = PyPDF2.PdfReader(arquivo)
+                    texto = ""
+                    for pagina in pdf.pages:
+                        texto += pagina.extract_text() + "\n"
+                print(texto)
+                return os.path.basename(caminho), texto
+
+            if caminho.endswith(".txt"):
+                with open(caminho, "r") as arquivo:
+                    texto = arquivo.read()
+                print(texto)
+                return os.path.basename(caminho), texto
+    except Exception as e:
+        return f"Erro ao carregar o arquivo: {e}"
 
 def fazer_prompt(modelo: str, prompt: str) -> str:
     print("[RODANDO PROMPT]")
@@ -33,10 +57,12 @@ def fazer_prompt(modelo: str, prompt: str) -> str:
             text=True,
             encoding="utf-8"
         )
-        for line in iter(resposta.stdout.readline, ''):
-            sys.stdout.write(line)  # envia para o terminal
-            sys.stdout.flush()  # garante que aparece imediatamente
-            yield line.rstrip()
+        while True:
+            char = resposta.stdout.read(1)
+
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            yield char
 
         resposta.stdout.close()
         resposta.wait()
