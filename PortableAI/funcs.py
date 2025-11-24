9ddd244f53efd2ad7bad7c12f9f1ffa1ff9e8306
João Alberto <138532454+jonh213412421@users.noreply.github.com>
2025-11-24 20:00:03 -0300

    Update funcs.py

--- PortableAI/funcs.py
+++ PortableAI/funcs.py
@@ -3,83 +3,128 @@ import os
 from tkinter import filedialog
 import sys
 import PyPDF2
+from langdetect import detect
+import spacy
 
-def iniciar() -> None:
-    if not os.path.exists(os.path.join(os.getcwd(), 'Llama')) or not os.path.exists(os.path.join(os.getcwd(), 'ícones')):
-        return "Erro! Falha na integridade dos diretórios."
 
-    if not os.path.isfile(os.path.join(os.getcwd(), 'Llama', 'llama-run.exe')):
-        return "Erro! llama-run.exe não está presente no diretório Llama."
+def iniciar() -> str:
 
-    if not os.path.exists(os.path.join(os.getcwd(), 'data')):
-        os.mkdir(os.path.join(os.getcwd(), 'data'))
+
+   if not os.path.exists(os.path.join(os.getcwd(), 'Llama')) or not os.path.exists(os.path.join(os.getcwd(), 'ícones')):
+       return "Erro! Falha na integridade dos diretórios."
+
+
+   if not os.path.isfile(os.path.join(os.getcwd(), 'Llama', 'llama-run.exe')):
+       return "Erro! llama-run.exe não está presente no diretório Llama."
+
+
+   if not os.path.exists(os.path.join(os.getcwd(), 'data')):
+       os.mkdir(os.path.join(os.getcwd(), 'data'))
+       return "Erro! diretório 'Data' não está presente"
+
 
 def listar_modelos() -> str:
-    return os.listdir("./Llama/Modelos")
+   return os.listdir("./Llama/Modelos")
+
 
 def selecionar_modelo(opcao) -> str:
-    if opcao == "Adicionar modelo":
-        return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
-    else:
-        print(f"Modelo selecionado: {opcao}")
-        return f"Modelo selecionado: {opcao}"
+   if opcao == "Adicionar modelo":
+       return filedialog.askopenfilename(initialdir="./Llama/Modelos", filetypes=(("model files", "*.gguf"),))
+   else:
+       print(f"Modelo selecionado: {opcao}")
+       return f"Modelo selecionado: {opcao}"
+
 
 def carregar_arquivo() -> ((str, str)):
-    try:
-        caminho = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("pdf", "*.pdf"), ("text files", "*.txt"),))
-        if caminho:
-            if caminho.endswith(".pdf"):
-                with open(caminho, "rb") as arquivo:
-                    pdf = PyPDF2.PdfReader(arquivo)
-                    texto = ""
-                    for pagina in pdf.pages:
-                        texto += pagina.extract_text() + "\n"
-                print(texto)
-                return os.path.basename(caminho), texto
+   try:
+       caminho = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("pdf", "*.pdf"), ("text files", "*.txt"),))
+       if caminho:
+           if caminho.endswith(".pdf"):
+               with open(caminho, "rb") as arquivo:
+                   pdf = PyPDF2.PdfReader(arquivo)
+                   texto = ""
+                   for pagina in pdf.pages:
+                       texto += pagina.extract_text() + "\n"
+               print(texto)
+               return os.path.basename(caminho), texto
 
-            if caminho.endswith(".txt"):
-                with open(caminho, "r") as arquivo:
-                    texto = arquivo.read()
-                print(texto)
-                return os.path.basename(caminho), texto
-    except Exception as e:
-        return f"Erro ao carregar o arquivo: {e}"
+
+           if caminho.endswith(".txt"):
+               with open(caminho, "r") as arquivo:
+                   texto = arquivo.read()
+               print(texto)
+               return os.path.basename(caminho), texto
+
+
+           else:
+               return "", "Tipo inválido de arquivo"
+       else:
+           return "", "Tipo inválido de arquivo"
+
+
+   except Exception as e:
+       return "--", f"Erro ao carregar o arquivo: {e}"
+
 
 def fazer_prompt(modelo: str, prompt: str) -> str:
-    print("[RODANDO PROMPT]")
+   print("[RODANDO PROMPT]")
 
-    dir_inicial = os.getcwd()
-    os.chdir(os.path.join(dir_inicial, "Llama"))
 
-    # Comando final
-    cmd = f"llama-run.exe .\\Modelos\\{modelo} -p {prompt}'"
+   dir_inicial = os.getcwd()
+   os.chdir(os.path.join(dir_inicial, "Llama"))
 
-    try:
-        resposta = subprocess.Popen(
-            cmd,
-            stdin=subprocess.DEVNULL, #-> RETORNA NULO PARA QUALQUER INPUT SOLICITADO DO USUÁRIO
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            shell=True,
-            text=True,
-            encoding="utf-8"
-        )
-        while True:
-            char = resposta.stdout.read(1)
 
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            yield char
+   # Comando final
+   cmd = f"llama-run.exe .\\Modelos\\{modelo} -p {prompt}'"
 
-        resposta.stdout.close()
-        resposta.wait()
-    except Exception as e:
-        print(f"Erro ao executar o modelo: {e}")
-        return "Erro!"
-    finally:
-        os.chdir(dir_inicial)
 
-print(listar_modelos())
+   try:
+       resposta = subprocess.Popen(
+           cmd,
+           stdin=subprocess.DEVNULL, #-> RETORNA NULO PARA QUALQUER INPUT SOLICITADO DO USUÁRIO
+           stdout=subprocess.PIPE,
+           stderr=subprocess.STDOUT,
+           shell=True,
+           text=True,
+           encoding="utf-8"
+       )
+
+
+       while True:
+           char = resposta.stdout.read(1)
+
+
+           sys.stdout.write(char)
+           sys.stdout.flush()
+           yield char
+
+
+       resposta.stdout.close()
+       resposta.wait()
+   except Exception as e:
+       print(f"Erro ao executar o modelo: {e}")
+       return "Erro!"
+   finally:
+       os.chdir(dir_inicial)
+
+
+def extrair_palavras_chave(prompt: str) -> list:
+   try:
+       if detect(prompt) == "pt":
+           nlp = spacy.load("pt_core_news_sm")
+           meta = nlp(prompt)
+       else:
+           nlp = spacy.load("en_core_web_sm")
+           meta = nlp(prompt)
+       palavras_filtradas = [token.text for token in meta if token.pos_ in ["NOUN", "ADJ", "ADV", "PRON", "PROPN"]]
+       return palavras_filtradas
+
+
+   except Exception as e:
+       print(f"Erro ao extrair palavras chave: {e}")
+
+
+extrair_palavras_chave("quem foi napoleão bonaparte?")
 # iniciar()
 #adicionar_modelo()
 #fazer_prompt("iu", "oi")
@@ -87,14 +132,6 @@ print(listar_modelos())
 #modelo = os.path.basename(modelo)
 #prompt = "você fala português?"
 #fazer_prompt(modelo, prompt)
-
-
-
-
-
-
-
-
 
 
 
